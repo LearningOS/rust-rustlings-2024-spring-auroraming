88eaec6e7a1bba34b682e76153dd9191331cb086
auroraming <auroraming@yeah.net>
2024-05-04 22:14:00 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num: i32) -> i32 {
+    if num == 35 {
+        return 70;
+    } else if num == 40 {
+        return 80;
+    } else if num == 41 {
+        return 41;
+    } else if num == 65 {
+        return 65;
+    }
+    0
+}
 
 // Don't modify this function!
 #[test]
